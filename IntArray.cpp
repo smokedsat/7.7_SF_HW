6e0e2b9b9ae4f89925ba32ac075b1e92acbf9b30
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-09-01 00:38:37 +0300

    Update IntArray.cpp

--- IntArray.cpp
+++ IntArray.cpp
@@ -1,198 +1,118 @@
 #include "IntArray.h"
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <algorithm>
 
-
-
-
-// Деструктор, освобождающий память
-IntArray::~IntArray()
-{
-	delete[] m_data;
+IntArray::IntArray(IntArray&& other) noexcept
+    : m_length(other.m_length), m_data(std::move(other.m_data)) {
+    other.m_length = 0;
 }
 
-// Удаляет созданный массив и 
-void IntArray::Erase()
-{
-	delete[]m_data;
-	m_data = nullptr;
-	m_length = 0;
+IntArray& IntArray::operator=(IntArray&& other) noexcept {
+    if (this != &other) {
+        m_length = other.m_length;
+        m_data = std::move(other.m_data);
+        other.m_length = 0;
+    }
+    return *this;
 }
 
-// Через данный оператор мы можем изменить элемент массива или получить его значение
-int& IntArray::operator[](int index)
-{
-	/*if ((index >= 0) && (index < m_length))
-	{
-		throw "Exception";
-	}*/
-	return m_data[index];
+IntArray::IntArray(int length) : m_length(length) {
+    if (length <= 0) {
+        throw std::invalid_argument("Length must be positive");
+    }
+    m_data = std::make_unique<int[]>(m_length);
 }
 
-// Функция возвращает длину массива
-int IntArray::getLength() const
-{
-	return m_length;
-};
+IntArray::~IntArray() {}
 
-// Функция позволяет изменить длину массива, выделив новую память под его элементы, но удалить все элементы массива перед этим. Функция работает быстро
-void IntArray::Reallocate(int newLength)
-{
-	if (newLength <= 0)
-	{
-		throw "Exception";
-	}
-	// Сначала удаляем все элементы и обнуляем указатели
-	Erase();
-
-	//Выделяем память под новые элементы
-	m_data = new int[newLength];
-	m_length = newLength;
-};
-
-void IntArray::Resize(int newSize)
-{
-	if (newSize <= 0)
-	{
-		throw "Exception";
-	}
-	if (newSize == m_length)
-	{
-		throw "Your array is already such size";
-	}
-
-	// Изменение размера массива будет производиться за счет копирования элементов массива в новый массив с измененным размером
-	//Создаем новый массив с новым размером
-	int* data = new int[newSize];
-
-	//Вычисляем, сколько элементов мы можем скопировать в новый массив
-	if (m_length > 0)
-	{
-		int elementsToCopy = 0;
-		if (newSize > m_length) { elementsToCopy = m_length; }
-		if (newSize < m_length) { elementsToCopy = newSize; }
-		//Копируем каждый элемент в новый массив
-		for (int index = 0; index < elementsToCopy; ++index)
-		{
-			data[index] = m_data[index];
-		}
-
-		//Заполняем новые элементы нулями.
-		for (int index = elementsToCopy; index < newSize; ++index)
-		{
-			data[index] = 0;
-		}
-	}
-
-	// Удаляем старый массив с данными, так как мы уже скопировали нужные данные из него
-	delete[] m_data;
-	m_data = data;
-	m_length = newSize;
+void IntArray::Erase() {
+    m_data.reset();
+    m_length = 0;
 }
 
-void IntArray::InsertBefore(int value, int index)
-{
-	if (index > m_length)
-	{
-		throw "Exception";
-	}
-	if (index < 0)
-	{
-		throw "Except";
-	}
-	// Создаем новый массив, который больше, чем прошлый
-	int* data = new int[m_length + 1];
-
-
-	// Копируем все элементы согласно старым индексам	
-	for (int before = 0; before < index; before++)
-	{
-		data[before] = m_data[before];
-	}
-
-	//Добавляем элемент в выделенный массив
-	data[index] = value;
-
-	//Копируем все значения после вставленного элемента
-	for (int after = index; after < m_length; ++after)
-	{
-		data[after + 1] = m_data[after];
-	}
-
-	// Удаляем прежний массив и заполняем новый
-	delete[]m_data;
-	m_data = data;
-	++m_length;
-	
+int& IntArray::operator[](int index) {
+    if (index < 0 || index >= m_length) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    return m_data[index];
 }
 
-void IntArray::RemoveElement(int index)
-{
-	if (index > m_length)
-	{
-		throw "Exception";
-	}
-	if (index < 0)
-	{
-		throw "Exception";
-	}
-
-	//Если это один элемент в массиве, просто удаляем массив целиком
-	if (m_length == 1)
-	{
-		Erase();
-		return;
-	}
-
-	// Создаем новый массив на 1 элемент меньше, чем прежний
-	int* data = new int[m_length - 1];
-
-	// Копируем все элементы поиндексально 
-	for (int before = 0; before < index; ++before)
-	{
-		data[before] = m_data[before];
-	}
-
-	// Копируем все значения после удаленного элемента
-	for (int after = index; after < m_length; ++after)
-	{
-		data[after - 1] = m_data[after];
-	}
-
-	// Удаляем старый массив и используем новый 
-	delete[] m_data;
-	m_data = data;
-	--m_length;
-	
-}
-void IntArray::findElementWithIndex(int index)
-{
-	if (index > m_length)
-	{
-		throw " Exception";
-	}
-	if (index < 0)
-	{
-		throw " Exception";
-	}
-	std::cout << m_data[index] << std::endl;
+int IntArray::getLength() const {
+    return m_length;
 }
 
-void IntArray::getArray()
-{
-	std::cout << "Array: " << std::endl;
-	for (int i = 0; i < getLength(); ++i)
-	{
-		std::cout << m_data[i] << ' ';
-	}
-	std::cout << std::endl;
+void IntArray::Reallocate(int newLength) {
+    if (newLength <= 0) {
+        throw std::invalid_argument("Incorrect length in reallocating");
+    }
+    m_data = std::make_unique<int[]>(newLength);
+    m_length = newLength;
 }
 
-void IntArray::insertAtBeginning(int value)
-{
-	InsertBefore(value, 0);
-}
-void IntArray::insertAtEnd(int value)
-{
-	InsertBefore(value, m_length);
+void IntArray::Resize(int newSize) {
+    if (newSize <= 0) {
+        throw std::invalid_argument("Incorrect size. It must be greater than 0.");
+    }
+    if (newSize == m_length) {
+        throw std::invalid_argument("Your array is already that size");
+    }
+    auto newData = std::make_unique<int[]>(newSize);
+    const int elementsToCopy = std::min(m_length, newSize);
+    std::copy(m_data.get(), m_data.get() + elementsToCopy, newData.get());
+    m_data = std::move(newData);
+    m_length = newSize;
 }
 
+void IntArray::InsertBefore(int value, int index) {
+    if (index < 0 || index > m_length) {
+        throw std::out_of_range("Invalid index for insertion");
+    }
+    auto newData = std::make_unique<int[]>(m_length + 1);
+    std::copy(m_data.get(), m_data.get() + index, newData.get());
+    newData[index] = value;
+    std::copy(m_data.get() + index, m_data.get() + m_length, newData.get() + index + 1);
+    m_data = std::move(newData);
+    ++m_length;
+}
+
+void IntArray::RemoveElement(int index) {
+    if (index < 0 || index >= m_length) {
+        throw std::out_of_range("Invalid index for removal");
+    }
+    if (m_length == 1) {
+        Erase();
+        return;
+    }
+    auto newData = std::make_unique<int[]>(m_length - 1);
+    std::copy(m_data.get(), m_data.get() + index, newData.get());
+    std::copy(m_data.get() + index + 1, m_data.get() + m_length, newData.get() + index);
+    m_data = std::move(newData);
+    --m_length;
+}
+
+
+// Example implementation for the new methods:
+int IntArray::findElementWithIndex(int index) const {
+    if (index < 0 || index >= m_length) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    return m_data[index];
+}
+
+void IntArray::printArray() const {
+    std::cout << "Array:" << std::endl;
+    for (int i = 0; i < getLength(); ++i) {
+        std::cout << m_data[i] << ' ';
+    }
+    std::cout << std::endl;
+}
+
+void IntArray::insertAtBeginning(int value) {
+    InsertBefore(value, 0);
+}
+
+void IntArray::insertAtEnd(int value) {
+    InsertBefore(value, m_length);
+}
