15d27051621a9fb2dc36037a0ca2e765cdd93736
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-09-01 00:37:51 +0300

    Update IntArray.h

--- IntArray.h
+++ IntArray.h
@@ -1,35 +1,38 @@
 #pragma once
+#include <memory>
 
-class IntArray 
-{
+class IntArray {
 private:
-	int m_length{};
-	int* m_data{};
+    int m_length{};
+    std::unique_ptr<int[]> m_data;
 
 public:
-	IntArray() = default;
-	// Конструктор, принимающй в себя целое число(длина массива)
-	IntArray(int length) : m_length(length)
-	{
-		/*// Исключение при длине массива <=0
-		if (length <= 0)
-		{
-			throw "Exception in the constructor";
-		}*/
-		// Заполнение ячеек массива длины length
-		m_data = new int [length] {};
-	};
+    IntArray() = default;
+    IntArray(int length);
+    ~IntArray();
 
-	~IntArray();
-	void Erase();
-	int& operator[](int index);
-	int getLength() const;
-	void Reallocate(int newLength);
-	void Resize(int newSize);
-	void InsertBefore(int value, int index);
-	void RemoveElement(int index);
-	void insertAtBeginning(int value);
-	void insertAtEnd(int value);
-	void findElementWithIndex(int index);
-	void getArray();
+    // Copy constructor
+    IntArray(const IntArray& other);
+
+    // Copy assignment operator
+    IntArray& operator=(const IntArray& other);
+
+    // Move constructor
+    IntArray(IntArray&& other) noexcept;
+
+    // Move assignment operator
+    IntArray& operator=(IntArray&& other) noexcept;
+
+    void Erase();
+    int& operator[](int index);
+    const int& operator[](int index) const; // Const version for read-only access
+    int getLength() const;
+    void Reallocate(int newLength);
+    void Resize(int newSize);
+    void InsertBefore(int value, int index);
+    void RemoveElement(int index);
+    void insertAtBeginning(int value);
+    void insertAtEnd(int value);
+    int findElementWithIndex(int index) const; // Return the found element
+    void printArray() const; // Renamed for clarity
 };
